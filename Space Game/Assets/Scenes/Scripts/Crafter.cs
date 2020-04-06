4fd32a22c1166d9ff3d48ed51b1842aab7118372
John Owens <jowens13@emich.edu>
2020-04-05 23:29:29 -0400

    Added level 1 stars, planets, asteroids, goal

---
+++ Space Game/Assets/Scenes/Scripts/Crafter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crafter : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+	public int numStars = 100;
+	
+	public GameObject starPrefab;
+	public Vector3 starPosMin = new Vector3(-50,-5);
+	public Vector3 starPosMax = new Vector3(150,100);
+	public float starScaleMin = 1;
+	public float starScaleMax = 3;
+	
+	private GameObject[] starInstances;
+	
+	void Awake(){
+		starInstances = new GameObject[numStars];
+		GameObject anchor = GameObject.Find("StarAnchor");
+		GameObject star;
+		for(int i=0; i<numStars; i++){
+			star = Instantiate<GameObject>(starPrefab);
+			
+			Vector3 cPos = Vector3.zero;
+			cPos.x = Random.Range(starPosMin.x, starPosMax.x);
+			cPos.y = Random.Range(starPosMin.y, starPosMax.y);
+			
+			float scaleU = Random.value;
+			float scaleVal = Mathf.Lerp(starScaleMin, starScaleMax, scaleU);
+			
+			cPos.y = Mathf.Lerp(starPosMin.y, cPos.y, scaleU);
+			
+			star.transform.position = cPos;
+			star.transform.localScale = Vector3.one * scaleVal;
+			
+			star.transform.SetParent(anchor.transform);
+			
+			starInstances[i] = star;
+		}
+
+	}
+}
---
+++ Space Game/Assets/Scenes/Scripts/Star.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+	[Header("Set in Inspector")]
+	public GameObject starSphere;
+	
+	public Vector3 sphereOffsetScale = new Vector3(0,0);
+	public Vector2 sphereScaleRangeX = new Vector2(1,1);
+	public Vector2 sphereScaleRangeY = new Vector2(1,1);
+	
+	private List<GameObject> spheres;
+	
+	void Start(){
+		spheres = new List<GameObject>();
+		
+
+		GameObject sp = Instantiate<GameObject>(starSphere);
+		spheres.Add(sp);
+		Transform spTrans = sp.transform;
+		spTrans.SetParent(this.transform);
+			
+		Vector3 offset = Random.insideUnitSphere;
+		offset.x *= sphereOffsetScale.x;
+		offset.y *= sphereOffsetScale.y;
+		spTrans.localPosition = offset;
+			
+		Vector3 scale = Vector3.one;
+		scale.x = Random.Range(sphereScaleRangeX.x, sphereScaleRangeX.y);
+		scale.y = scale.x;
+		
+		//scale.y = Random.Range(sphereScaleRangeY.x, sphereScaleRangeY.y);
+		//scale.y *= 1 - (Mathf.Abs(offset.x) / sphereOffsetScale.x);
+		//scale.y = Mathf.Max(scale.y, scaleYMin);
+			
+		spTrans.localScale = scale;
+		
+	}
+	
+	void Restart(){
+		foreach(GameObject sp in spheres){
+			Destroy(sp);
+		}
+		
+		Start();
+	}
+	
+	
+}
